0257fa46af0d1c0d80880db39b48cda2dde96acd
Guilherme Ferreira <106194282+guixande0905g@users.noreply.github.com>
2023-10-06 08:06:10 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 let num1 = "";
 let num2 = "";
 let valor = "";
-let temPonto = false;
+let temPonto = false; 
+let executa = "";
 mensagem = (msg) => console.log(msg);
 soma = (a,b) => a + b;
 sub = (a,b) => a - b;
@@ -12,21 +13,39 @@ montante_juro_simples = (c,j) => soma(juros_simples(c,j),c);
 
 mostrar_display = (msg)=>{
   document.getElementById("resultado").value = msg;
-  
 }
 
 function digitando(tecla){
   if(tecla == "."){
-    if (!temPonto){
-    valor += tecla;
-    mostrar_display(valor);
-     temPonto = true;
+    if(!temPonto){
+      valor += tecla;
+      mostrar_display(valor);
+      temPonto = true;
     }
     return;
   }
    valor += tecla;
    mostrar_display(valor);
-   
+}
+function operacao(op){
+    executa = op;
+    num1 = valor;
+    valor = "";
+}
+
+function calcula(){
+   if(executa != ""){
+     num2 = valor;
+     if(executa == "soma") mostrar_display(soma(Number(num1),Number(num2)));
+     if(executa == "sub") mostrar_display(sub(num1,num2));
+     if(executa == "div") mostrar_display(div(num1,num2));
+     if(executa == "mult") mostrar_display(mult(num1,num2));
+     num1 = "";
+     num2 = "";
+     valor = "";
+     temPonto = false;
+     executa = "";
+   }
 }
 
 
